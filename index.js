dab12345728b0b015f7220596beec78a1955c237
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-09-30 20:52:12 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.port || 8000
+const path = require('path')
+const { send } = require('process')
+
+app.use(express.urlencoded({extended: true})) 
+
+const http = require('http').createServer(app)
+
+// app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', (req, res) => {
+  res.send("start")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`running on port ${PORT}`);
+})
